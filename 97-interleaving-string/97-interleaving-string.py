cb297aeaa50e3c28576a747f3dbe14d177814e90
Naman Rajput <naman17121998@gmail.com>
2022-07-07 19:27:19 +0530

    Time: 68 ms (46.09%), Space: 14 MB (90.66%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -5,28 +5,17 @@ class Solution:
         
         @lru_cache(None)
         def solve(i, j, k):
-            # print(f'called on {i, j, k}')
             if i == 0 and j == 0:
-                if k == 0:
-                    return True
-                else:   return False
+                return True if k == 0 else False
             elif k == 0:
                 return False
             
-            if i == 0 and s2[j-1] == s3[k-1]:
-                return solve(i, j-1, k-1)
+            if i == 0:
+                return True if s2[0:j] == s3[0:k] else False
             
-            if j == 0 and s1[i-1] == s3[k-1]:
-                return solve(i-1, j, k-1)
+            if j == 0:
+                return True if s1[0:i] == s3[0:k] else False
             
-            if i != 0 and j != 0:
-                if s1[i-1] == s2[j-1] == s3[k-1]:
-                    return solve(i-1, j, k-1) or solve(i, j-1, k-1)
-                elif s1[i-1] == s3[k-1]:
-                    return solve(i-1, j, k-1)
-                elif s2[j-1] == s3[k-1]:
-                    return solve(i, j-1, k-1)
-            
-            return False
+            return (s1[i-1] == s3[k-1] and solve(i-1, j, k-1)) or (s2[j-1] == s3[k-1] and solve(i, j-1, k-1))
         
         return solve(len(s1), len(s2), len(s3))
