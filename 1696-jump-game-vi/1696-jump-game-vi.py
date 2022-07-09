3bc7759f6636d412f745f21e20b0f03f63ebb9ab
Naman Rajput <naman17121998@gmail.com>
2022-07-09 16:28:20 +0530

    Time: 2060 ms (18.25%), Space: 33.4 MB (30.66%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,16 +1,14 @@
+from heapq import heappush, heappop
+
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        dq = deque([0])  # store index of `nums` elements, elements is in decreasing order, the front is the maximum element.
-        for i in range(1, n):
-            # nums[i] = max(nums[i-k], nums[i-k+1],.., nums[i-1]) + nums[i] = nums[dq.front()] + nums[i]
-            nums[i] = nums[dq[0]] + nums[i]
+        highestK = [(-nums[0],0)]
+        result = nums[0]  # handles case where there is just 1 itme
+        
+        for i in range(1,len(nums)):
+            while highestK[0][1] < i-k:
+                heapq.heappop(highestK) # Pop the score whose index is out of range of i
+            result = nums[i] + -highestK[0][0]  # Add highest possible score
+            heapq.heappush(highestK, (-result,i))   # Append to heap
 
-            # Add a nums[i] to the deq
-            while dq and nums[dq[-1]] <= nums[i]: dq.pop()  # Eliminate elements less or equal to nums[i], which will never be chosen in the future
-            dq.append(i)
-
-            # Remove if the last element is out of window size k
-            if i - dq[0] >= k: dq.popleft()
-
-        return nums[n - 1]
+        return result
