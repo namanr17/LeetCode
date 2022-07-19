151fb34d55182d7bca858df23a8cce5193cf06dd
Naman Rajput <naman17121998@gmail.com>
2022-07-20 00:50:32 +0530

    Time: 1748 ms (79.15%), Space: 78.6 MB (80.90%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,13 +1,6 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        adj = defaultdict(list)
-        
-        for u in range(n):
-            for v in range(n):
-                if u != v:
-                    w = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
-                    adj[u] += [(v, w)]
         
         q = [(0, 0)]
         visited = [False] * n
@@ -19,12 +12,9 @@ class Solution:
                 (w, u) = heappop(q)
             visited[u] = True
             cost += w
-            for (v, w) in adj[u]:
+            for v in range(n):
                 if not visited[v]:
+                    w = abs(points[v][0] - points[u][0]) + abs(points[v][1] - points[u][1])
                     heappush(q, (w, v))
-                
-        # print(visited)
-        # print(adj)
-        # print(cost)
         
         return cost
