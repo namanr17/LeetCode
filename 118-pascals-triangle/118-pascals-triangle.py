a712f55ad4f5621a26cab8aa29c46241a36a17f5
Naman Rajput <naman17121998@gmail.com>
2022-07-19 17:31:07 +0530

    Time: 52 ms (37.85%), Space: 13.9 MB (17.88%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,6 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(1, numRows):
+            res += [list(map(lambda x, y: x+y, res[-1] + [0], [0] + res[-1]))]
+        return res[:numRows]
