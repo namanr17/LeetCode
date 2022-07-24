ebf00612f66dbca5d92ed6a4202f810886644606
Naman Rajput <naman17121998@gmail.com>
2022-07-24 19:02:57 +0530

    Time: 346 ms (13.32%), Space: 20.3 MB (84.45%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int)-> bool:
+        if not len(matrix) or not len(matrix[0]):
+			# Quick response for empty matrix
+            return False
+        
+        h, w = len(matrix), len(matrix[0])
+        
+        # Start adaptive search from bottom left corner
+        y, x = h-1, 0
+        
+        while True:
+            if y < 0 or x >= w:
+                break
+            
+            current = matrix[y][x]
+            if target < current:                    # target is smaller, then go up
+                y -= 1
+            elif target > current:                  # target is larger, then go right
+                x += 1
+            else:   return True                     # target hit
+            
+        return False
+            
+            
