f65436a8375dc85c07b2297be94d07420c2e0437
Naman Rajput <naman17121998@gmail.com>
2022-07-30 18:21:40 +0530

    Time: 2105 ms (10.01%), Space: 18.6 MB (54.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,19 +1,19 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        words2_max_freq = defaultdict(int)
+        def count(word):
+            ans = [0] * 26
+            for letter in word:
+                ans[ord(letter) - ord('a')] += 1
+            return ans
+        
+        words2_max_freq = [0] * 26
         for word in words2:
-            for c, freq in Counter(word).items():
+            for c, freq in enumerate(count(word)):
                 words2_max_freq[c] = max(words2_max_freq[c], freq)
                 
         ans = []
         for word in words1:
-            flag = True
-            wordFreq = Counter(word)
-            for c, freq in words2_max_freq.items():
-                if freq > wordFreq[c]:
-                    flag = False
-                    break
-            if flag:
+            if all(x >= y for x, y in zip(count(word), words2_max_freq)):
                 ans.append(word)
         
         return ans
