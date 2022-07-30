a72f9dad93705e7e0aecadbeac20b2c6840f5c8b
Naman Rajput <naman17121998@gmail.com>
2022-07-31 02:10:53 +0530

    Time: 826 ms (86.51%), Space: 18.3 MB (22.04%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,18 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n, nums = len(nums), sorted(nums)
-        numSet, ans = {}, set([])
+        numSet, ans = {}, []
         
         for idx, num in enumerate(nums):
-            numSet[num] = idx
+            if num in numSet:
+                numSet[num][1] = idx
+            else:   numSet[num] = [idx, idx]
             
         for i in range(n-2):
             if nums[i] > 0: break
-            for j in range(i+1, n-1):
-                reqSum = -(nums[i] + nums[j])
-                if reqSum in numSet.keys() and numSet[reqSum] > j:
-                    ans.add(tuple(sorted([nums[i], nums[j], reqSum])))
-                j = numSet[nums[j]]
-            i = numSet[nums[i]]
-        
+            if i > 0 and nums[i] == nums[i-1]:  continue
+            l, r = i+1, n-1
+            while(l < r):
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l = numSet[nums[l]][1] + 1
+                elif s > 0:
+                    r = numSet[nums[r]][0] -1
+                else:
+                    ans.append([nums[i],nums[l],nums[r]])
+                    l = numSet[nums[l]][1] + 1
+                    r = numSet[nums[r]][0] - 1
+            i = numSet[nums[i]][1]
+            
         return ans
