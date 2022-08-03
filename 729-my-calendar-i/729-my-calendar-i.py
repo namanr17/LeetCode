ce1a11af1854430d0b5ea4498b822df59db61948
Naman Rajput <naman17121998@gmail.com>
2022-08-04 01:18:30 +0530

    Time: 380 ms (64.75%), Space: 14.9 MB (29.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,10 +1,38 @@
-class MyCalendar(object):
+class MyCalendar:
     def __init__(self):
-        self.booking = []
+        self.calendar = []
+		# Use Linked List instead, much more faster
+		# self.calender = collections.deque()
 
-    def book(self, start, end):
-        for i, j in self.booking:
-            if i < end and start < j:
+    def book(self, start: int, end: int) -> bool:
+        right = len(self.calendar)
+        if right == 0:
+            self.calendar.append((start, end))
+            return True
+
+        left = 0
+        while left < right:
+            mid = int(left + (right - left)/2)
+            if self.calendar[mid][1] <= start:
+                left = mid + 1
+            else:
+                right = mid
+
+        if left == 0:
+            if self.calendar[left][0] >= end:
+                self.calendar.insert(0, (start, end))
+                return True
+            else:
                 return False
-        self.booking.append((start, end))
-        return True
+        elif left == len(self.calendar):
+            if self.calendar[left - 1][0] <= start:
+                self.calendar.append((start, end))
+                return True
+            else:
+                return False
+                
+        if left - 1 >= 0 and self.calendar[left - 1][1] <= start and self.calendar[left][0] >= end:
+            self.calendar.insert(left, (start, end))
+            return True
+            
+        return False
