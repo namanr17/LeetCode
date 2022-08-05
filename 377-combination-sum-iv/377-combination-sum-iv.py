7c1206ba5df4e55371cf25d8e12ade415196e8ca
Naman Rajput <naman17121998@gmail.com>
2022-08-05 17:35:27 +0530

    Time: 82 ms (19.62%), Space: 14 MB (48.19%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,19 +1,9 @@
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        # from functools import lru_cache
-        
-        @lru_cache(None)
-        def solve(t):
-            if t < 0:
-                return 0
-            
-            if t == 0:
-                return 1
-            
-            ret = 0
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        nums, combs = sorted(nums), [1] + [0] * (target)
+        for i in range(target + 1):
             for num in nums:
-                ret += solve(t - num)
-            
-            return ret
-        
-        return solve(target)
+                if num  > i: break
+                if num == i: combs[i] += 1
+                if num  < i: combs[i] += combs[i - num]
+        return combs[target]
