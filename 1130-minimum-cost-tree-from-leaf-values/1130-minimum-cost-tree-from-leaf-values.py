a2fe9282718c37298799044586ff4da4bb4e1344
Naman Rajput <naman17121998@gmail.com>
2022-08-07 21:55:47 +0530

    Time: 64 ms (54.67%), Space: 14 MB (48.32%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -1,21 +1,29 @@
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
+        n = len(arr)
+        ngl = [None] * n
+        ngr = [None] * n
         
-        @lru_cache(None)
-        def solve(l, r):
-            if l == r:
-                return 0, arr[l]
-            
-            minCost, maxEle = float('inf'), 0
-            for k in range(l, r):
-                cost_l, max_l = solve(l, k)
-                cost_r, max_r = solve(k+1, r)
-                
-                cost = max_l * max_r + cost_l + cost_r
-                if cost < minCost:
-                    minCost = cost
-                    maxEle = max(max_l, max_r)
-            
-            return minCost, maxEle
+        stack_ngl = deque()
+        stack_ngr = deque()
         
-        return solve(0, len(arr)-1)[0]
+        for i in range(n):
+            while(stack_ngl and stack_ngl[-1] <= arr[i]):
+                stack_ngl.pop()
+            ngl[i] = stack_ngl[-1] if stack_ngl else float('inf')
+            stack_ngl.append(arr[i])
+            
+            j = n - i - 1
+            while(stack_ngr and stack_ngr[-1] < arr[j]):
+                stack_ngr.pop()
+            ngr[j] = stack_ngr[-1] if stack_ngr else float('inf')
+            stack_ngr.append(arr[j])
+            
+        print(ngl, ngr)
+        
+        cost = 0
+        for i in range(n):
+            p = min(ngl[i], ngr[i])
+            cost += arr[i] * p if p != float('inf') else 0
+            
+        return cost
