fae6f72f4ca334262a30aa5e8bf037b4ce59f4cd
Naman Rajput <naman17121998@gmail.com>
2022-08-14 23:18:08 +0530

    Time: 1068 ms (12.86%), Space: 15 MB (82.87%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,29 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        charList = [char for char in string.ascii_lowercase]
+        
+        if endWord not in wordList:
+            return 0
+        
+        n = len(wordList)
+        nodes = set(wordList + [beginWord])
+        queue = deque([beginWord])
+        ret = 0
+        
+        while(queue):
+            ret += 1
+            
+            workingSet = set(queue.copy())
+            queue.clear()
+            
+            for word in workingSet:
+                nodes.remove(word)
+                if word == endWord:
+                    return ret
+                for i in range(len(word)):
+                    for char in charList:
+                        destWord = word[:i] + char + word[i+1:]
+                        if destWord in nodes and destWord not in workingSet:
+                            queue.append(destWord)
+                            
+        return 0
