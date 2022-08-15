1c2446f7208c20d76ec8e6a055c5caeaeae6db6b
Naman Rajput <naman17121998@gmail.com>
2022-08-15 23:12:41 +0530

    Time: 55 ms (30.60%), Space: 13.9 MB (90.33%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -3,23 +3,25 @@ class Solution:
         heap = [(-a, 'a'), (-b, 'b'), (-c, 'c')] 
         heapify(heap)
         
-        ret = []
+        ret = deque()
         while(heap):
             freq, char = heappop(heap)
             freq = -freq
             if len(ret) < 2 or ret[-1] != char:
                 l = min(freq, 2)
-                ret += [char] * l
-                freq -= l
+                while(l):
+                    ret.append(char)
+                    l -= 1
+                    freq -= 1
             
             if not heap:    break
             
             freq2, char2 = heappop(heap)
             if freq2:
-                ret += [char2]
+                ret.append(char2)
                 freq2 += 1
                 heappush(heap, (freq2, char2))
             
             if freq:    heappush(heap, (-freq, char))
                 
-        return ''.join(ret)
+        return ''.join(list(ret))
