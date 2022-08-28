9d1ef85b79ceadc2907f108374109f915dcb5dbd
Naman Rajput <naman17121998@gmail.com>
2022-08-28 17:52:26 +0530

    Time: 182 ms (13.89%), Space: 14.2 MB (76.31%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,13 +1,25 @@
 class Solution:
-    def diagonalSort(self, A):
-        n, m = len(A), len(A[0])
-        d = collections.defaultdict(list)
-        for i in range(n):
-            for j in range(m):
-                d[i - j].append(A[i][j])
-        for k in d:
-            d[k].sort(reverse=1)
-        for i in range(n):
-            for j in range(m):
-                A[i][j] = d[i - j].pop()
-        return A
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        buckets = Counter()
+        
+        for d in range(1-n, m):
+            i, j = (0, -d) if d < 0 else (d, 0)
+            
+            while(i < m and j < n):
+                buckets[mat[i][j]] += 1
+                i += 1
+                j += 1
+                
+            i, j = (0, -d) if d < 0 else (d, 0)
+            k = 1
+            
+            while(k <= 100):
+                while(buckets[k]):
+                    mat[i][j] = k
+                    buckets[k] -= 1
+                    i += 1
+                    j += 1
+                k += 1
+            
+        return mat
