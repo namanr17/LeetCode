baf7a2d3d9db0f2b0dbf28ac4a8ef870dd014e10
Naman Rajput <naman17121998@gmail.com>
2022-08-31 12:59:09 +0530

    Time: 286 ms (95.05%), Space: 15.6 MB (78.42%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,32 +1,24 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows, cols = len(heights), len(heights[0])
-        
-        def dfs_pacific(x, y):            
-            toPacific.add((x, y))
+            
+        def dfs(x, y, visited):            
+            visited.add((x, y))
             
             for (r, c) in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
                 if 0 <= r < rows and 0 <= c < cols and heights[r][c] >= heights[x][y]:
-                    if (r, c) not in toPacific:
-                        dfs_pacific(r, c)
-            
-        def dfs_atlantic(x, y):            
-            toAtlantic.add((x, y))
-            
-            for (r, c) in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
-                if 0 <= r < rows and 0 <= c < cols and heights[r][c] >= heights[x][y]:
-                    if (r, c) not in toAtlantic:
-                        dfs_atlantic(r, c)
+                    if (r, c) not in visited:
+                        dfs(r, c, visited)
               
         toPacific = set()
         toAtlantic = set()
                         
         for r in range(rows):
-            dfs_pacific(r, 0)
-            dfs_atlantic(r, cols-1)
+            dfs(r, 0, toPacific)
+            dfs(r, cols-1, toAtlantic)
             
         for c in range(cols):
-            dfs_pacific(0, c)
-            dfs_atlantic(rows-1, c)
+            dfs(0, c, toPacific)
+            dfs(rows-1, c, toAtlantic)
                     
         return list(toPacific & toAtlantic)
