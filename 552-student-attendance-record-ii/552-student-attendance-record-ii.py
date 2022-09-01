b99ee1396ccfe8716835daaffbcc1ec3bc281c3c
Naman Rajput <naman17121998@gmail.com>
2022-09-02 00:59:20 +0530

    Time: 3313 ms (19.08%), Space: 25.6 MB (21.17%) - LeetHub

--- 552-student-attendance-record-ii/552-student-attendance-record-ii.py
+++ 552-student-attendance-record-ii/552-student-attendance-record-ii.py
@@ -4,20 +4,23 @@ class Solution:
             return 1
         if n == 1:
             return 3
-        
-        dp = [None for _ in range(n+1)]
-        dp[0] = 1
-        dp[1] = 2
-        dp[2] = 4
+        if n == 2:
+            return 8
         
         MOD = int(1e9) + 7
         
+        P = [None] * (n+1)
+        A = [None] * (n+1)
+        L = [None] * (n+1)
+        
+        P[0], A[0], L[0] = 1, 1, 1
+        P[1], A[1], L[1] = 1, 1, 1
+        P[2], A[2], L[2] = 3, 2, 3
+        
         for i in range(3, n+1):
-            dp[i] =  (dp[i-1] + dp[i-2] + dp[i-3]) % MOD
+            A[i] = ((A[i-1] + A[i-2]) % MOD + A[i-3]) % MOD
+            P[i] = ((P[i-1] + L[i-1]) % MOD + A[i-1]) % MOD
+            L[i] = (((P[i-1] + A[i-1]) % MOD + P[i-2]) % MOD + A[i-2]) % MOD
             
-        ret = dp[n]
-        for i in range(n):
-            ret += (dp[i] * dp[n-i-1]) % MOD
-            ret = ret % MOD
-            
-        return ret
+        return ((A[n] + P[n]) % MOD + L[n]) % MOD
+        
