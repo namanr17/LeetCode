05cce05f14cd5e38b9c0bb3795b63c06d6f16b46
Naman Rajput <naman17121998@gmail.com>
2022-09-02 20:24:47 +0530

    Time: 98 ms (13.41%), Space: 14.3 MB (32.27%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -6,36 +6,19 @@
 #         self.right = right
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:    
-        count = 0
+        self.count = 0
+        covered = {None}
         
-        def dfs(node):
-            # 0 -> uncovered
-            # 1 -> covered by a neighbour's camera
-            # 2 -> covered by own camera
+        def dfs(node, paren=None):
+            if node.left:   dfs(node.left, node)
+            if node.right:  dfs(node.right, node)
             
-            nonlocal count
-            
-            # Leaf node is marked as uncovered
-            if not node.left and not node.right:
-                return 0
-            
-            # Consider child as covered by a neighbour if NULL
-            left = dfs(node.left) if node.left else 1
-            right = dfs(node.right) if node.right else 1
-            
-            # Place a camera if either child is uncovered
-            if left == 0 or right == 0:
-                count += 1
-                return 2
-            
-            # No need to place a camera if both children are covered
-            if left == 1 and right == 1:
-                return 0
-            
-            # Node is covered if there's a camera at either child
-            if left == 2 or right == 2:
-                return 1
+            if node.left not in covered or node.right not in covered:
+                self.count += 1
+                covered.update({node, paren, node.left, node.right})
+                
+        dfs(root)
+        if root not in covered:
+            self.count += 1
         
-        if dfs(root) == 0:
-            count += 1
-        return count
+        return self.count
