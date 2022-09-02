d35513632fd5787fabf0311c7567ff6612471e37
Naman Rajput <naman17121998@gmail.com>
2022-09-03 00:34:35 +0530

    Time: 69 ms (8.98%), Space: 13.9 MB (32.80%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.py
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.py
@@ -1,22 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        
-        def solve(node1, node2):
-            if not node1 and not node2:
-                return True
-            
-            if not node1 or not node2:
-                return False
-            
-            if node1.val != node2.val:
-                return False
-            
-            return solve(node1.left, node2.left) and solve(node1.right, node2.right) or solve(node1.left, node2.right) and solve(node1.right, node2.left)
-        
-        return solve(root1, root2)
+class Solution(object):
+    def flipEquiv(self, root1, root2):
+        if root1 is root2:
+            return True
+        if not root1 or not root2 or root1.val != root2.val:
+            return False
+
+        return (self.flipEquiv(root1.left, root2.left) and
+                self.flipEquiv(root1.right, root2.right) or
+                self.flipEquiv(root1.left, root2.right) and
+                self.flipEquiv(root1.right, root2.left))
