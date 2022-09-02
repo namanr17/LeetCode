a7788245b582f7e47abdb9fdcfd5e67a02cfcb83
Naman Rajput <naman17121998@gmail.com>
2022-09-02 14:43:33 +0530

    Time: 5651 ms (5.03%), Space: 47.7 MB (88.37%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        rows, cols = len(points), len(points[0])
+        
+        if rows == 1:   return max(points[0])
+        
+        prev = points[0]
+        for r in range(1, rows):
+            dp, left, right = [None] * cols, [None] * cols, [None] * cols
+            
+            left[0] = prev[0]
+            for i in range(1, cols):
+                left[i] = max(left[i-1]-1, prev[i])
+            
+            right[-1] = prev[-1]
+            for i in range(cols-2, -1, -1):
+                right[i] = max(right[i+1]-1, prev[i])
+            
+            for i in range(cols):
+                dp[i] = points[r][i] + max(left[i], right[i])
+            prev = dp
+            
+        return max(dp)
