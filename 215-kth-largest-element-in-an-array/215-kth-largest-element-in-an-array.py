6cc5a1e9ebd21162023ba52404dba4de8a745330
Naman Rajput <naman17121998@gmail.com>
2022-09-10 03:34:26 +0530

    Time: 1293 ms (22.58%), Space: 27.1 MB (81.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,29 +1,30 @@
 class Solution:
     def findKthLargest(self, nums, k):
+        return sorted(nums, reverse=True)[k-1]
         
-        def RandomPart(l, r):
-            p = random.randint(l, r)
-            nums[p], nums[r] = nums[r], nums[p]
+#         def RandomPart(l, r):
+#             p = random.randint(l, r)
+#             nums[p], nums[r] = nums[r], nums[p]
             
-            m = l
-            for i in range(l, r):
-                if nums[i] < nums[r]:
-                    nums[m], nums[i] = nums[i], nums[m]
-                    m += 1
+#             m = l
+#             for i in range(l, r):
+#                 if nums[i] < nums[r]:
+#                     nums[m], nums[i] = nums[i], nums[m]
+#                     m += 1
             
-            nums[m], nums[r] = nums[r], nums[m]
-            return m
+#             nums[m], nums[r] = nums[r], nums[m]
+#             return m
         
-        def QuickSelect(lo, hi, k):
-            if lo == hi:    return nums[lo]
+#         def QuickSelect(lo, hi, k):
+#             if lo == hi:    return nums[lo]
             
-            pivot = RandomPart(lo, hi)
-            rank = pivot - lo + 1
+#             pivot = RandomPart(lo, hi)
+#             rank = pivot - lo + 1
             
-            if k < rank:
-                return QuickSelect(lo, pivot - 1, k)
-            elif k > rank:
-                return QuickSelect(pivot + 1, hi, k - rank)
-            else:   return nums[pivot]
+#             if k < rank:
+#                 return QuickSelect(lo, pivot - 1, k)
+#             elif k > rank:
+#                 return QuickSelect(pivot + 1, hi, k - rank)
+#             else:   return nums[pivot]
             
-        return QuickSelect(0, len(nums)-1, len(nums)-k+1)
+#         return QuickSelect(0, len(nums)-1, len(nums)-k+1)
