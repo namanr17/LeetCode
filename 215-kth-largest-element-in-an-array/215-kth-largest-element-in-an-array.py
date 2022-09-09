eefa3260496a5b54ea00971d360a4e1762b280f2
Naman Rajput <naman17121998@gmail.com>
2022-09-10 01:22:36 +0530

    Time: 7496 ms (5.00%), Space: 123.8 MB (5.72%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,21 +1,25 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        pivot = random.choice(nums)
-        
-        M = 0
-        left, right = [], []
-        
-        for n in nums:
-            if n > pivot:
-                left.append(n)
-            elif n < pivot:
-                right.append(n)
-            else:   M += 1
-        
-        L = len(left)
-        
-        if k <= L:
-            return self.findKthLargest(left, k)
-        elif k > L + M:
-            return self.findKthLargest(right, k - L - M)
-        else:   return pivot
+    def findKthLargest(self, nums, k):
+        # convert the kth largest to smallest
+        return self.findKthSmallest(nums, len(nums)+1-k)
+
+    def findKthSmallest(self, nums, k):
+        if nums:
+            pos = self.partition(nums, 0, len(nums)-1)
+            if k > pos+1:
+                return self.findKthSmallest(nums[pos+1:], k-pos-1)
+            elif k < pos+1:
+                return self.findKthSmallest(nums[:pos], k)
+            else:
+                return nums[pos]
+
+    # choose the right-most element as pivot   
+    def partition(self, nums, l, r):
+        low = l
+        while l < r:
+            if nums[l] < nums[r]:
+                nums[l], nums[low] = nums[low], nums[l]
+                low += 1
+            l += 1
+        nums[low], nums[r] = nums[r], nums[low]
+        return low
