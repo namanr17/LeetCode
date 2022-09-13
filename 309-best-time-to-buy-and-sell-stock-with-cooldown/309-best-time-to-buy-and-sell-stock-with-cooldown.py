cffee291bf7f8e2b380dc5b31cc1cb5954c99f47
Naman Rajput <naman17121998@gmail.com>
2022-09-14 04:25:29 +0530

    Time: 99 ms (17.28%), Space: 14 MB (99.89%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        @lru_cache(None)
-        def trade(day, hold):
-            if day >= len(prices):  return 0
-            
-            if hold:
-                return max(prices[day] + trade(day+2, False), trade(day+1, True))
-            else:   return max(-prices[day] + trade(day+1, True), trade(day+1, False))
+        hold, sold, rest = -inf, 0, 0
         
-        return trade(0, False)
+        for price in prices:
+            prev_sold, prev_hold, prev_rest = sold, hold, rest
+            
+            rest = max(prev_sold, prev_rest)
+            sold = max(prev_hold + price, prev_sold)
+            hold = max(prev_rest - price, prev_hold)
+            
+        return max(sold, rest)
