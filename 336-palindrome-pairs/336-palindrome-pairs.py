1db0a0018334978414bafd1ea8a14bebec60eb32
Naman Rajput <naman17121998@gmail.com>
2022-09-22 05:27:13 +0530

    Time: 3529 ms (68.41%), Space: 26.3 MB (74.68%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.py
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -2,26 +2,25 @@ class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
         ret = []
         
-        hs = {word: idx for idx, word in enumerate(words)}
+        hs = {word[::-1]: idx for idx, word in enumerate(words)}
         
         for i, word in enumerate(words):
             if not word:    continue
+                
             if '' in hs and word[::-1] == word:
                 ret.append([i, hs['']])
                 ret.append([hs[''], i])
-            if word[::-1] in hs:
-                j = hs[word[::-1]]
-                if i != j:
-                    ret.append([i, j])
+                
+            if word in hs and i != hs[word]:
+                ret.append([i, hs[word]])
+                
             for k in range(1, len(word)):
                 pref, suff = word[:k], word[k:]
-                if pref[::-1] in hs:
-                    j = hs[pref[::-1]]
-                    if suff == suff[::-1]:
-                        ret.append([i, j])
-                if suff[::-1] in hs:
-                    j = hs[suff[::-1]]
-                    if pref == pref[::-1]:
-                        ret.append([j, i])
+                
+                if pref in hs and suff == suff[::-1]:
+                    ret.append([i, hs[pref]])
+                        
+                if suff in hs and pref == pref[::-1]:
+                    ret.append([hs[suff], i])
         
         return ret
