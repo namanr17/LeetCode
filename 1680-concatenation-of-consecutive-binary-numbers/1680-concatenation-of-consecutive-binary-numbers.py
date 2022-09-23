45e0356fd91a94e773d1965918ebb32dbfe5ee1a
Naman Rajput <naman17121998@gmail.com>
2022-09-23 23:02:15 +0530

    Time: 6972 ms (5.51%), Space: 116.3 MB (5.51%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -1,3 +1,5 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
-        return (self.concatenatedBinary(n-1) << n.bit_length() | n) % int(1e9 + 7) if n > 1 else 1
+        if n == 1:  return 1
+        
+        return (self.concatenatedBinary(n-1) << n.bit_length() | n) % int(1e9 + 7)
