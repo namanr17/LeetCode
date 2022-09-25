cc23e9e8ffa2e5ee290a7a2b01f92eb406ef536a
Naman Rajput <naman17121998@gmail.com>
2022-09-25 18:44:46 +0530

    Time: 65 ms (98.61%), Space: 14.4 MB (85.14%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.py
+++ 622-design-circular-queue/622-design-circular-queue.py
@@ -1,45 +1,41 @@
 class MyCircularQueue:
-
     def __init__(self, k: int):
-        self.k = k
-        self.arr = [None] * k
-        self.start = 0
-        self.end = 0
+        self.size = 0
+        self.max_size = k
+        self.front = 0
+        self.rear = -1
+        self.queue = [0] * k
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False
+        else:
+            self.rear = (self.rear + 1) % self.max_size
+            self.queue[self.rear] = value
+            self.size += 1
+            return True
         
-        self.arr[self.end] = value
-        self.end = (self.end + 1) % self.k
-        return True
-    
+
     def deQueue(self) -> bool:
         if self.isEmpty():
             return False
+        else:
+            self.front = (self.front+1) % self.max_size
+            self.size -= 1
+            return True
         
-        self.arr[self.start] = None
-        self.start = (self.start + 1) % self.k
-        return True
 
     def Front(self) -> int:
-        return self.arr[self.start] if self.arr[self.start] != None else -1
+        return self.queue[self.front] if self.size else -1
+        
 
     def Rear(self) -> int:
-        return self.arr[self.end - 1] if self.arr[self.end - 1] != None else -1
+        return self.queue[self.rear] if self.size else -1
+        
 
     def isEmpty(self) -> bool:
-        return self.start == self.end and self.arr[self.start] == None
+        return self.size == 0
+        
 
     def isFull(self) -> bool:
-        return self.start == self.end and self.arr[self.start] != None
-
-
-# Your MyCircularQueue object will be instantiated and called as such:
-# obj = MyCircularQueue(k)
-# param_1 = obj.enQueue(value)
-# param_2 = obj.deQueue()
-# param_3 = obj.Front()
-# param_4 = obj.Rear()
-# param_5 = obj.isEmpty()
-# param_6 = obj.isFull()
+        return self.size == self.max_size
