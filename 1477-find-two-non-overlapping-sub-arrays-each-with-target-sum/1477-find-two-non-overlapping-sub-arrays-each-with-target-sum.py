de3f8235ef14052c92b5c02de82b9cec24d17ae7
Naman Rajput <naman17121998@gmail.com>
2022-09-30 07:27:01 +0530

    Time: 2345 ms (21.81%), Space: 27.9 MB (68.56%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -1,19 +1,16 @@
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
-        prefSum, winSum = {0: -1}, 0
-        ans, best, best_till = inf, inf, [inf] * len(arr)
-        
-        for j in range(len(arr)):
-            winSum += arr[j]
-            
-            if winSum - target in prefSum:
-                i = prefSum[winSum - target]
-                if i >= 0:
-                    ans = min(ans, j - i + best_till[i])
-                best = min(best, j - i)
-            
-            best_till[j] = best
-            prefSum[winSum] = j
-        
-        return -1 if ans == inf else ans
-            
+        i, window, result = 0, 0, float('inf')
+        premin = [float('inf')] * len(arr)
+        for j, num in enumerate(arr):
+            window += num
+            while window > target:
+                window -= arr[i]
+                i += 1
+            if window == target:
+                curr = j - i + 1
+                result = min(result, curr + premin[i - 1])
+                premin[j] = min(curr, premin[j - 1])
+            else:
+                premin[j] = premin[j - 1]
+        return result if result < float('inf') else -1
