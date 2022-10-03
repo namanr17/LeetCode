de82e39f5404aaa796b955cbf260d95d58565474
Naman Rajput <naman17121998@gmail.com>
2022-10-04 04:08:05 +0530

    Time: 2847 ms (20.76%), Space: 25.1 MB (27.04%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,16 +1,9 @@
 class Solution:
-    def minCost(self, colors: str, neededTime: List[int]) -> int:
-        time = 0
-        max_time = 0
-        
-        for i in range(len(colors)):
-            if i > 0 and colors[i] != colors[i-1]:
-                max_time = 0
-            
-            time += min(max_time, neededTime[i])
-            max_time = max(max_time, neededTime[i])
-            
-        return time
-                
-                
-                
+    def minCost(self, s: str, cost: List[int]) -> int:
+        ans = prev = 0 # index of previously retained letter 
+        for i in range(1, len(s)): 
+            if s[prev] != s[i]: prev = i
+            else: 
+                ans += min(cost[prev], cost[i])
+                if cost[prev] < cost[i]: prev = i
+        return ans
