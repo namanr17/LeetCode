13d93aa827920e94bbe9bb19f79d717c75d8c66b
Naman Rajput <naman17121998@gmail.com>
2022-10-11 01:11:44 +0530

    Time: 152 ms (99.49%), Space: 14.4 MB (12.55%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,28 +1,43 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        if len(nums) == 3:
-            return sum(nums)
-        
-        ans, diff = inf, inf
         nums.sort()
+        return self.kSumClosest(nums, 3, target)
         
-        for i, x in enumerate( nums[:-2] ):
-            lo, hi = i+1, len(nums)-1
-            
-            curr = 0
-            while lo < hi:
-                curr = nums[lo] + nums[hi] + x
+    def kSumClosest(self, nums, k, target):
+        N = len(nums)
+        # Special case where we only have k elements in nums. Return only option
+        if N == k:
+            return sum(nums[:k])
+        
+        # special case where the target is way too low
+        # we give the lowest we can
+        if sum(nums[:k]) >= target:
+            return sum(nums[:k])
+        
+        # special case where the target is too high
+        # we give the largest we can
+        if sum(nums[-k:]) <= target:
+            return sum(nums[-k:])
+        
+        # base case. look for the closest element
+        if k == 1:
+            # the element is the first and the delta is the second
+            deltas = [(x, abs(target-x)) for x in nums]
+            return min(deltas, key = lambda x: x[1])[0]
+        
+        # pick one element out and recursively search for closest match with k being one less
+        closest = sum(nums[:k])
+        for i,x in enumerate(nums):
+            # small optimization to handle duplicate x values
+            if i>0 and nums[i-1] == x:
+                continue
                 
-                if curr > target:
-                    hi -= 1
-                    while lo < hi and nums[hi] == nums[hi+1]:   hi -= 1
-                elif curr < target:
-                    lo += 1
-                    while lo < hi and nums[lo] == nums[lo-1]:   lo += 1
+            bestMatch = self.kSumClosest(nums[i+1:], k-1, target-x)
+            current = x + bestMatch
+            if abs(target-current) < abs(target-closest):
+                if target == current:
+                    return current
                 else:
-                    return curr
-                
-                if abs( target - curr ) < diff:
-                    ans, diff = curr, abs( target - curr )
-        
-        return ans
+                    closest = current
+                    
+        return closest
