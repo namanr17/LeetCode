6fbf8634e0a00c2b3ab47918edd0a3362b0fee87
Naman Rajput <naman17121998@gmail.com>
2022-10-12 07:12:05 +0530

    Time: 1859 ms (28.62%), Space: 14.1 MB (70.13%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,4 +1,4 @@
 class Solution:
-    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        hm = Counter(num1 + num2 for num1 in nums1 for num2 in nums2)
-        return sum(hm[-num3-num4] for num3 in nums3 for num4 in nums4)
+    def fourSumCount(self, A, B, C, D):
+        AB = collections.Counter(a+b for a in A for b in B)
+        return sum(AB[-c-d] for c in C for d in D)
