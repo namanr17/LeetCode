d1c7efc891806761d8cdf92434e03556933e50ec
Naman Rajput <naman17121998@gmail.com>
2022-10-12 07:11:32 +0530

    Time: 1956 ms (24.66%), Space: 14.1 MB (70.13%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,15 +1,4 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        count = 0
-        hm = defaultdict(int)
-        
-        for x in nums1:
-            for y in nums2:
-                hm[x + y] += 1
-            
-        
-        for x in nums3:
-            for y in nums4:
-                count += hm[-(x+y)]
-        
-        return count
+        hm = Counter(num1 + num2 for num1 in nums1 for num2 in nums2)
+        return sum(hm[-num3-num4] for num3 in nums3 for num4 in nums4)
