92215e5adaff062de317b6e7eaf448a9aad8dd74
Naman Rajput <naman17121998@gmail.com>
2022-10-18 09:17:41 +0530

    Time: 102 ms (30.59%), Space: 14 MB (54.87%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -1,18 +1,8 @@
 class Solution:
-    @lru_cache(None)
-    def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        
-        say_prev = self.countAndSay(n-1)
-        ret = ''
-        count = 1
-        for i in range(1, len(say_prev)):
-            if say_prev[i] != say_prev[i-1]:
-                ret += str(count) + say_prev[i-1]
-                count = 1
-            else:   count += 1
-        
-        return ret + str(count) + say_prev[-1]
+    def countAndSay(self, n):
+        s = '1'
+        for _ in range(n - 1):
+            s = ''.join(str(len(list(group))) + digit for digit, group in itertools.groupby(s))
+        return s
             
         
